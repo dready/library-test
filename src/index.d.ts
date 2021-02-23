a74a7ea326ce81389f5a31af133d2862c006a6d0
Thomas Subera <thomas.subera@gmail.com>
2021-02-23 15:50:09 +0100

    quick setup for an example library

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+export enum EnumExample {
+  Example1 = "example1",
+  Example2 = "example2",
+}
+
+export interface InterfaceExample {
+  x: number;
+  y: number;
+}
+
+export type TypeExample = string | number;
